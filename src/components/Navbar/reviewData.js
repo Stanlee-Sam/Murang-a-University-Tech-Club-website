1468a06c9c56978b4287d145266b5d6a247ff5bf
stanleyamunze03@gmail.com <stanleyamunze03@gmail.com>
2024-08-29 13:01:15 +0300

    image path commit

--- src/components/Navbar/reviewData.js
+++ src/components/Navbar/reviewData.js
@@ -2,35 +2,35 @@ export const reviews = [
   {
     id: 1,
     name: "John Morgan",
-    img: "/dist/testimonial students/student1.jpeg",
+    img: "/public/testimonialStudents/student1.jpeg",
     review:
       "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aliquid perferendis reiciendis voluptate delectus mollitia ut facilis, beatae, eius officia quos autem maxime voluptates eligendi assumenda similique! Praesentium est blanditiis quo?",
   },
   {
     id: 2,
     name: "Ellie Anderson",
-    img: "/public/testimonial students/student2.jpeg",
+    img: "/public/testimonialStudents/student2.jpeg",
     review:
       "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aliquid perferendis reiciendis voluptate delectus mollitia ut facilis, beatae, eius officia quos autem maxime voluptates eligendi assumenda similique! Praesentium est blanditiis quo?",
   },
   {
     id: 3,
     name: "Sean Abedayo",
-    img: "/public/testimonial students/student3.jpeg",
+    img: "/public/testimonialStudents/student3.jpeg",
     review:
       "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aliquid perferendis reiciendis voluptate delectus mollitia ut facilis, beatae, eius officia quos autem maxime voluptates eligendi assumenda similique! Praesentium est blanditiis quo?",
   },
   {
     id: 4,
     name: "Rigo Abedayo",
-    img: "/public/testimonial students/student4.jpeg",
+    img: "/public/testimonialStudents/student4.jpeg",
     review:
       "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aliquid perferendis reiciendis voluptate delectus mollitia ut facilis, beatae, eius officia quos autem maxime voluptates eligendi assumenda similique! Praesentium est blanditiis quo?",
   },
   {
     id: 5,
     name: "Nia Morgan",
-    img: "/public/testimonial students/student5.jpeg",
+    img: "/public/testimonialStudents/student5.jpeg",
     review:
       "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aliquid perferendis reiciendis voluptate delectus mollitia ut facilis, beatae, eius officia quos autem maxime voluptates eligendi assumenda similique! Praesentium est blanditiis quo?",
   },
